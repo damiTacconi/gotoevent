fd5abb692ca5446bcf3f06b76f20d8d65d3cb85c
Damian <dami_tano_95@hotmail.com>
2018-10-14 00:19:17 -0300

    mejorado la vista

--- Vista/comprarEvento.php
+++ Vista/comprarEvento.php
@@ -1,105 +1,120 @@
 <?php
 $bgT = false;
-include "navbar.php" ?>
-<?php
+include "navbar.php";
+include "navCategorias.php";
+
 $evento = $param['evento'];
 $imagen = $evento->getEventoImagen()->getImagen();
 $url = "data:image/jpg;base64,{$imagen}";
 
-
 ?>
-<div class="container-fluid">
-    <?php if($evento->getCategoria()->getDescripcion() === "Festival") {
-        $precioPromo = 0;
-        foreach ($param['calendarios'] as $calendario){
+
+<section class="container-fluid pt-3">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="<?= $url ?>" class="w-100">
+                        </div>
+                        <div class="col-md-8 px-3">
+                            <div class="card-block p-3">
+                                <h2 class="card-title"><?= $evento->getTitulo() ?></h2>
+                                <hr>
+                                <p class="card-text"> <?= $evento->getDescripcion() ?> </p>
+                                <a href="#" class="btn btn-primary">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><?php
+            if($evento->getCategoria()->getDescripcion() === "Festival") {
+            $precioPromo = 0;
+            foreach ($param['calendarios'] as $calendario){
             $plazas = $calendario->getPlazaEventos();
             foreach ($plazas as $plaza){
-                $precioPromo += $plaza->getPrecio();
+            $precioPromo += $plaza->getPrecio();
             }
-        }
-        $precioPromo -= 500;
-        ?>
-        <div class="row justify-content-center">
-            <div class="col-12 p-5">
-                <div class="card text-center" >
-                    <div class="card-header success-color white-text">
-                        <h1><?= $evento->getTitulo() ?></h1>
+            }
+            $precioPromo -= 500;
+            ?>
+            <div class="col-md-4">
+                    <div class="card text-center" >
+                        <div class="card-header success-color white-text">
+                            <h1><?= $evento->getTitulo() ?></h1>
+                        </div>
+                        <div class="card-body">
+                            <h2 class="card-title">PACK PROMO $<?= $precioPromo ?></h2>
+                            <p class="card-text">No te pierdas esta promo.</p>
+                            <button type="button" class="btn btn-success">COMPRAR</button>
+                        </div>
+                        <div class="card-footer text-muted success-color white-text">
+                            <p class="mb-0">Compra los <?= count($param['calendarios']) ?> Dias</p>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h2 class="card-title">PACK PROMO $<?= $precioPromo ?></h2>
-                        <p class="card-text">No te pierdas esta promo.</p>
-                          <button type="button" class="btn btn-success">COMPRAR</button>
-                    </div>
-                    <div class="card-footer text-muted success-color white-text">
-                        <p class="mb-0">Compra los <?= count($param['calendarios']) ?> Dias</p>
-                    </div>
-                </div>
             </div>
+            <?php } ?>
         </div>
-    <?php } ?>
-    <div class="row ">
-        <div class="col-12 col-md-3">
-            <!--Panel-->
-            <div class="card border-primary mt-5 mb-3" style="max-width: 18rem;">
-                <div class="card-header"><?= $evento->getTitulo() ?></div>
-                <div class="card-body text-primary">
-                <img src="<?= $url ?>" width="250" height="200" alt="Imagen">
+</section>
 
-                    <p class="card-text p-3"> <?= $evento->getDescripcion() ?></p>
-                </div>
-            </div>
-            <!--/.Panel-->
-        </div>
-        <div class="col-md-9">
+    <div class="row">
+        <div class="col-md-12 p-4">
             <?php
                 if($param['calendarios']) { ?>
-                    <div class="row"> <?php
-                    foreach ($param['calendarios'] as $calendario) {
-                        $plazas = $calendario->getPlazaEventos();
-                         ?>
-
-                         <?php
-                        if ($plazas) {
-                                foreach ($plazas as $plaza) {
+                    <div class="card">
+                        <div class="card-header primary-color lighten-1 white-text">
+                            <h1>SEDE</h1>
+                        </div>
+                        <div class="card-body">
+                            <div class="row justify-content-center"> <?php
+                                foreach ($param['calendarios'] as $calendario) {
+                                    $plazas = $calendario->getPlazaEventos();
                                     ?>
-                                    <div class="col-md-4 col-12">
-                                        <div class="p-3">
-                                            <div class="card">
-                                                <h3 class="card-header primary-color white-text text-center"><?= $calendario->getFecha() ?> </h3>
-                                                <div class="card-body">
-                                                    <ul class="list-group list-group-flush">
-                                                        <li class="list-group-item">PLAZA:
-                                                            <strong>
-                                                                <?= $plaza->getPlaza()->getDescripcion() ?>
-                                                            </strong>
-                                                        </li>
-                                                        <li class="list-group-item">PRECIO: <strong>
-                                                                $<?= $plaza->getPrecio() ?> </strong></li>
-                                                        <li class="list-group-item">
-                                                            <form class="form" method="post" action="/compra/addToCart/">
-                                                                <input type="hidden" name="idPlaza"
-                                                                       value="<?= $plaza->getId() ?>">
-                                                                <div class="form-group">
-                                                                    <label for="inputCantidad">Cantidad</label>
-                                                                    <input required value="1" type='number'
-                                                                           class="form-control" min='1' max='5'
-                                                                           id="inputCantidad" name='cantidad'>
-                                                                </div>
-                                                                <button type="submit" class="btn btn-primary">AGREGAR
-                                                                </button>
-                                                            </form>
-                                                        </li>
-                                                    </ul>
+                                    <?php
+                                    if ($plazas) {
+                                        foreach ($plazas as $plaza) {
+                                            ?>
+                                            <div class="col-md-auto col-12">
+                                                <div class="p-1">
+                                                    <div class="card">
+                                                        <h3 class="card-header primary-color white-text text-center"><?= $calendario->getFecha() ?> </h3>
+                                                        <div class="card-body">
+                                                            <ul class="list-group list-group-flush">
+                                                                <li class="list-group-item">PLAZA:
+                                                                    <strong>
+                                                                        <?= $plaza->getPlaza()->getDescripcion() ?>
+                                                                    </strong>
+                                                                </li>
+                                                                <li class="list-group-item">PRECIO: <strong>
+                                                                        $<?= $plaza->getPrecio() ?> </strong></li>
+                                                                <li class="list-group-item">
+                                                                    <form class="form" method="post" action="/compra/addToCart/">
+                                                                        <input type="hidden" name="idPlaza"
+                                                                               value="<?= $plaza->getId() ?>">
+                                                                        <div class="form-group">
+                                                                            <label for="inputCantidad">Cantidad</label>
+                                                                            <input required value="1" type='number'
+                                                                                   class="form-control" min='1' max='5'
+                                                                                   id="inputCantidad" name='cantidad'>
+                                                                        </div>
+                                                                        <button type="submit" class="btn btn-primary">AGREGAR
+                                                                        </button>
+                                                                    </form>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <?php
-                                }
-                        } else echo " <br/> NO HAY PLAZAS DE MOMENTO ...<br/>";
-                        echo " <br/> ";
-                    } ?>
-                    </div> <?php
+                                            <?php
+                                        }
+                                    } else echo " <br/> NO HAY PLAZAS DE MOMENTO ...<br/>";
+                                    echo " <br/> ";
+                                } ?>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
                 }else echo " <br/> <h1>NO HAY CALENDARIOS DE MOMENTO ...</h1> "?>
         </div>
 
---
+++ Vista/navCategorias.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-dark blue scrolling-navbar">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">TEATROS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">CONCIERTOS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">FESTIVALES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">MAS</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 bg-white">
+            <div id="containerSearch" class="border-bottom p-3 border-light">
+                <form  class="justify-content-center form-inline active-cyan-4">
+                    <input class="form-control form-control-sm mr-3 w-50" type="text" placeholder="ENCONTRA LO MEJOR DEL ENTRETENIMIENTO" aria-label="Search">
+                    <button id="btnSearch" class="btn btn-primary btn-sm" style="width: 10%"><i class="fa fa-search" aria-hidden="true"></i></button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
